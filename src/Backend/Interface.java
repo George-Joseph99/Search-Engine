ed76b7014541dd0ad1099379ef8307a2776db6f1
George-Joseph99 <george_joseph99@hotmail.com>
2022-05-19 02:57:21 +0200

    html

--- src/Backend/Interface.java
+++ src/Backend/Interface.java
@@ -12,26 +12,32 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
 
+@WebServlet("/Interface")
 public class Interface extends HttpServlet {
     private static MongoDatabase database;
     private static MongoClient mongoClient;
-    public String path = "C:\\Users\\George\\Desktop\\Search-Engine\\Server\\apache-tomcat-9.0.63\\webapps\\ROOT";
-
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        /*
         mongoClient = new MongoClient(); //index table
         database = mongoClient.getDatabase("indexDb"); // index database
         MongoCollection<Document> collection = database.getCollection("index_table"); // index collection
-        String input = request.getParameter("input");
+        */
+        String name = request.getParameter("Name");
+        String gender = request.getParameter("Gender");
+        String message = "Your name is " + name + " gender is: " + gender;
+        response.setContentType("text/html");
+        String page = "<!doctype html> <html> <body> <h1> " + message + "</h1> </body> </html>";
+        response.getWriter().println(page);
         //queryprocessor qp = new queryprocessor();
         //ArrayList<Words> docs;
-        if (input != null) {
+       // if (input != null) {
             //String[] final_stemmed_array =
             //docs = qp.non_phrase_search(input);
             // relevanceranker rr = new relevanceranker();
             // rr.calculate_relevance(docs);
-            response.setContentType("text/html");
-            PrintWriter out = response.getWriter();
-            }
+            //response.setContentType("text/html");
+            //PrintWriter out = response.getWriter();
+           // }
         }
     }
 
